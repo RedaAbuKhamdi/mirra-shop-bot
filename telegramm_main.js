bfd0ba1fe432fae76266cf7df2b34396741c584d
Reda <rabouhamdi@gmail.com>
2022-09-19 16:07:22 +0200

    ads

--- telegramm_main.js
+++ telegramm_main.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.json())
 const port = 3000
 let chatId = {
   id:[],
